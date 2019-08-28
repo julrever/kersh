98f19a667a819089ea61f054ecc077731999a6a1
aes3plex <aes3plex@ya.ru>
2019-08-28 14:52:09 +0300

    menu fixed

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 from lxml import html
 import requests
-
+from requests_html import HTMLSession
 
 def get_links():
     r = requests.get('https://guz.ru/postuplenie/priemnaya-kampaniya-2019-2020/rez-vstup-ispyt/', verify=False)
     html_content = html.fromstring(r.content)
     return html_content.xpath('//td[@class="workarea"]/div/a/@href')
+
+
+session = HTMLSession()
+r = session.get('http://art-afrika.ru/lunch/')
+pic = r.html.find('#dle-content', first=True).find('a')[1].attrs['href']
+print(pic)
--- utils/food.py
+++ utils/food.py
@@ -16,7 +16,7 @@ def send_menu(bot, chat_id):
         except:
             session = HTMLSession()
             r = session.get('http://art-afrika.ru/lunch/')
-            pic = r.html.find('#dle-content', first=True).find('a', first=True).attrs['href']
+            pic = r.html.find('#dle-content', first=True).find('a')[1].attrs['href']
             f = open('africa/' + today + '.jpg', 'wb')
             f.write(urllib.request.urlopen(pic).read())
             f.close()
@@ -25,8 +25,10 @@ def send_menu(bot, chat_id):
     img.close()
 
 
+
 def where_to_eat(bot, chat_id):
     where = random.choice(PLACES_TO_EAT)
     bot.send_message(chat_id, where, parse_mode='Markdown', disable_web_page_preview=True)
     if where == 'Африка':
         send_menu(bot, chat_id)
+
