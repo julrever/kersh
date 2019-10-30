8466e0c223ad6c23eb7cc7f584faab2e664284a3
aes3plex <aes3plex@ya.ru>
2019-10-30 18:41:51 +0300

    time changed

--- main.py
+++ main.py
@@ -165,11 +165,11 @@ def days_til_freedom():
     while True:
         time_now = datetime.datetime.now(datetime.timezone.utc).strftime('%H:%M:%S')[:5]
         if time_now == '06:00':
-            bot.send_message(-1001378368946, random.choice(('Утро в хату', 'Дарова', 'Керш в здании')) +
-                             random.choice((', пацаны и пацанессы', ', собаки')) + '!')
-            '''weather_day(bot, -1001378368946, 0)'''
-            weekday = datetime.datetime.today().weekday()
             if weekday != 5 and weekday != 6:
+                bot.send_message(-1001378368946, random.choice(('Утро в хату', 'Дарова', 'Керш в здании')) +
+                                 random.choice((', пацаны и пацанессы', ', собаки')) + '!')
+                '''weather_day(bot, -1001378368946, 0)'''
+                weekday = datetime.datetime.today().weekday()
                 bot.send_message(-1001378368946, random.choice(('Меню Африки:', 'А можно меню Африки? - спросите вы')))
                 send_menu(bot, -1001378368946)
                 bot.send_message(-1001378368946, random.choice(('Хорошего дня, пидоры <3', 'Не благодарите',
@@ -189,7 +189,7 @@ def days_til_freedom():
             if days < -10:
                 time.sleep(10)
                 bot.send_message(-1001378368946, 'Пожалуйста')'''
-        elif time_now == '16:00':
+        elif time_now == '18:30':
             '''bot.send_message(-1001378368946, 'Вечер в хату, арестанты! Погодка на завтра:')
             weather_day(bot, -1001378368946, 1)'''
             bot.send_message(-1001378368946, 'Денис, сука, где скетчи?')
